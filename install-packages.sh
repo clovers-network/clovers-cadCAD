e45cb9a7b2df63b5a4d1864983707e3879e6c0e0
okwme <billy.rennekamp@gmail.com>
2019-10-18 13:03:23 +0200

    works on fresh install

--- install-packages.sh
+++ install-packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source .env
-pip install --extra-index-url https://$GEMFURY_TOKEN:@repo.fury.io/blockscience/ -r requirements.txt
+pip install -r requirements.txt
 
 # git clone https://github.com/rkern/line_profiler.git && find line_profiler -name '*.pyx' -exec cython {} \; && cd line_profiler && pip install . && cd .. && rm -rf line_profiler
